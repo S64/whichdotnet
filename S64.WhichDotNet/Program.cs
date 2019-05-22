cd947f62786f13da7ee043f492ecf125d1fabb2b
Shuma Yoshioka <s64.stdio@gmail.com>
2019-05-22 11:41:31 +0900

    Init project

---
+++ S64.WhichDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S64.WhichDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
