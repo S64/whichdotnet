98fed1a9698cae759c9112544428096f0a03bc40
Shuma Yoshioka <s64.stdio@gmail.com>
2019-05-22 18:32:21 +0900

    Create pathes

---
+++ S64.WhichDotNet.Core/Pathes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace S64.WhichDotNet.Core
+{
+
+    public sealed class Pathes
+    {
+
+        private static readonly char PathSeparator
+            = IsWindows ? ';' : ':';
+
+        private static string PathEnv
+        {
+            get { return Environment.GetEnvironmentVariable("PATH"); }
+        }
+
+        private static readonly bool IsWindows
+            = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+    }
+
+}
