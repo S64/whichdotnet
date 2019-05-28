5d71df81016e862999f702747509884d631d87f6
Shuma Yoshioka <s64.stdio@gmail.com>
2019-05-28 22:08:34 +0900

    Move to which

--- S64.WhichDotNet.Core/Pathes.cs
+++ S64.WhichDotNet.Core/Pathes.cs
@@ -21,8 +21,8 @@ namespace S64.WhichDotNet.Core
         private static readonly bool IsWindows
             = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
-        private static readonly char DSC
-            = Path.DirectorySeparatorChar;
+        public static readonly char DSC
+                    = Path.DirectorySeparatorChar;
 
 
         public IEnumerable<DirectoryInfo> GetOrderedPathDirectories(bool skipDot = false, bool skipTilde = false)
@@ -32,18 +32,6 @@ namespace S64.WhichDotNet.Core
                 .Where(x => !skipDot || x.IndexOf(".", StringComparison.Ordinal) != 0)
                 .Where(x => !skipTilde || x.IndexOf("~", StringComparison.Ordinal) != 0)
                 .Select(x => new DirectoryInfo(x));
-        }
-
-        public IEnumerable<FileInfo> FindProgramFromPath(IEnumerable<DirectoryInfo> dirs, string program)
-        {
-            if (string.IsNullOrWhiteSpace(program))
-            {
-                return Enumerable.Empty<FileInfo>();
-            }
-            return dirs
-                .Where(x => x.Exists)
-                .Select(x => new FileInfo($"{x.FullName}{DSC}{program}"))
-                .Where(x => x.Exists);
         }
 
     }
---
+++ S64.WhichDotNet.Core/Which.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace S64.WhichDotNet.Core
+{
+
+    public sealed class Which
+    {
+
+        public IEnumerable<FileInfo> FindProgramFromPath(IEnumerable<DirectoryInfo> dirs, string program)
+        {
+            if (string.IsNullOrWhiteSpace(program))
+            {
+                return Enumerable.Empty<FileInfo>();
+            }
+            return dirs
+                .Where(x => x.Exists)
+                .Select(x => new FileInfo($"{x.FullName}{Pathes.DSC}{program}"))
+                .Where(x => x.Exists);
+        }
+
+    }
+
+}
