fc21c9c5db49e04accd5eab0496c0507b7abbf22
Shuma Yoshioka <s64.stdio@gmail.com>
2019-05-28 22:47:57 +0900

    Impl GetRegPaths

--- S64.WhichDotNet.WinExt/WinExt.cs
+++ S64.WhichDotNet.WinExt/WinExt.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Microsoft.Win32;
+using System.Linq;
 
 namespace S64.WhichDotNet.WinExt
 {
@@ -8,7 +10,39 @@ namespace S64.WhichDotNet.WinExt
     public sealed class WinExt
     {
 
+        private static List<RegistryKey> Keys
+        {
+            get { return new List<RegistryKey> { Registry.CurrentUser, Registry.LocalMachine } }
+        }
 
+        public static IEnumerable<FileInfo> GetRegPaths()
+        {
+            var items = new List<FileInfo>();
+
+            foreach (var root in Keys)
+            {
+                using (var paths = OpenSubKey(root))
+                {
+                    foreach (var keyName in paths.GetSubKeyNames())
+                    {
+                        using (var item = paths.OpenSubKey(keyName))
+                        {
+                            if (item.GetValue("") is string programPath && !string.IsNullOrWhiteSpace(programPath))
+                            {
+                                items.Add(new FileInfo(programPath));
+                            }
+                        }
+                    }
+                }
+            }
+
+            return items;
+        }
+
+        private static RegistryKey OpenSubKey(RegistryKey key)
+        {
+            return key.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\App Paths");
+        }
 
     }
 
