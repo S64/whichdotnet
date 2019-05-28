e1985d205dbe494aaff92e56f30562f605788fc7
Shuma Yoshioka <s64.stdio@gmail.com>
2019-05-28 22:02:36 +0900

    Impl FindProgram

--- S64.WhichDotNet.Core/Pathes.cs
+++ S64.WhichDotNet.Core/Pathes.cs
@@ -21,15 +21,31 @@ namespace S64.WhichDotNet.Core
         private static readonly bool IsWindows
             = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
+        private static readonly char DSC
+            = Path.DirectorySeparatorChar;
+
 
         public IEnumerable<DirectoryInfo> GetOrderedPathDirectories(bool skipDot = false, bool skipTilde = false)
         {
             return PathEnv.Split(PathSeparator)
-                .Where(x => !string.IsNullOrEmpty(x))
+                .Where(x => !string.IsNullOrWhiteSpace(x))
                 .Where(x => !skipDot || x.IndexOf(".", StringComparison.Ordinal) != 0)
                 .Where(x => !skipTilde || x.IndexOf("~", StringComparison.Ordinal) != 0)
                 .Select(x => new DirectoryInfo(x));
         }
+
+        public IEnumerable<FileInfo> FindProgram(IEnumerable<DirectoryInfo> dirs, string program)
+        {
+            if (string.IsNullOrWhiteSpace(program))
+            {
+                return Enumerable.Empty<FileInfo>();
+            }
+            return dirs
+                .Where(x => x.Exists)
+                .Select(x => new FileInfo($"{x.FullName}{DSC}{program}"))
+                .Where(x => x.Exists)
+        }
+
     }
 
 }
